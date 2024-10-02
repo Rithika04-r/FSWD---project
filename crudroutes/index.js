612bd24acff966c2e2d7f78c132ea349b61a709a
Karthikadevi017 <sekarp994438@gmail.com>
2024-10-02 22:30:10 +0530

    crudroutes

---
+++ crudroutes/index.js
@@ -0,0 +1,20 @@
+import Router from "express";
+import Book from "../..models/book.js";
+const router = Router();
+router.post("/go", async (req, res) => {
+  try {
+    const book = Book(req.body);
+    await book.save();
+    res.send("Library Management created");
+  } catch (error) {
+    res.send(error);
+  }
+});
+router.get("/get1", async (req, res) => {
+  try {
+    const book = await Book.find();
+    res.send(book);
+  } catch (error) {
+    console.log(error);
+  }
+});
